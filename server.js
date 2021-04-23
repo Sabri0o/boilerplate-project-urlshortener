1a16f7c4ad13086fc1b978f71237d5ccc1a98f5d
Sabri0o <sabritrabelsi90@gmail.com>
2021-04-23 16:22:40 +0100

    connecting to database

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+// connecting to database
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (error) {
+  console.log(error);
+}
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  function (error) {
+    if (error) {
+      console.log("Database error or database connection error " + error);
+    }
+    console.log("Database state is " + !!mongoose.connection.readyState);
+  }
+);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
